f7b4fe57bf6e3ba7d1aa0dd2c3ae9fe2bdb2b55f
Vannie2024 <163892771+Vannie2024@users.noreply.github.com>
2026-06-23 19:20:39 +0100

    aadded frontend url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,7 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://poimenexams.onrender.com"],
+    origin: ["http://localhost:5173", "https://poimenexams.vercel.app"],
     credentials: true,
 }));
 app.use(express.json());
